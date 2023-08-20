9d284befbec2cd43dfbd7c0229c32a2b4d91a618
mnmandahalf <manaminakamura812@gmail.com>
2023-08-20 15:49:46 +0900

    remove activerecord require

--- lib/strong_migrations.rb
+++ lib/strong_migrations.rb
@@ -101,7 +101,6 @@ ActiveSupport.on_load(:active_record) do
 
   require_relative "strong_migrations/schema_dumper"
   ActiveRecord::SchemaDumper.prepend(StrongMigrations::SchemaDumper)
-  require "active_record/connection_adapters/mysql2_adapter"
   ActiveRecord::ConnectionAdapters::Mysql2Adapter
     .prepend(StrongMigrations::ConnectionAdapters::Mysql2Adapter)
 end
