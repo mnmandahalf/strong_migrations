2f69ecd159105d316ef927a36e80bf628b7af1ea
mnmandahalf <manaminakamura812@gmail.com>
2023-08-20 15:34:04 +0900

    fix

--- lib/strong_migrations/connection_adapters/mysql2_adapter.rb
+++ lib/strong_migrations/connection_adapters/mysql2_adapter.rb
@@ -2,16 +2,16 @@ module StrongMigrations
   module ConnectionAdapters
     module Mysql2Adapter
       def execute(sql, name = nil)
-        add_lock_none!(sql)
+        sql = add_lock_none(sql)
         super(sql, name)
       end
   
 
       private
 
-      def add_lock_none!(sql)
+      def add_lock_none(sql)
         if StrongMigrations.force_lock_none && sql.upcase.include?('ALTER TABLE')
-          sql = "#{sql.chomp(";")}, LOCK=NONE;"
+          "#{sql.chomp(";")}, LOCK=NONE;"
         end
       end
     end
