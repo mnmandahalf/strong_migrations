4b9835f7c84562de9eed47d835e2cbabd4652ad2
mnmandahalf <manaminakamura812@gmail.com>
2023-08-20 20:40:39 +0900

    add target sql

--- lib/strong_migrations/connection_adapters/mysql2_adapter.rb
+++ lib/strong_migrations/connection_adapters/mysql2_adapter.rb
@@ -14,17 +14,29 @@ module StrongMigrations
       end
 
       def add_algorithm_instant(sql)
-        if StrongMigrations.force_instant_ddl && sql.upcase.include?('ALTER TABLE')
+        if StrongMigrations.force_instant_ddl && is_target_for_check_online_ddl?(sql)
+          sql = sql.gsub(/,\s*ALGORITHM\s*=\s*\w+\s*/i, '').squeeze(' ').strip
           sql = "#{sql.chomp(";")}, ALGORITHM=INSTANT;"
         end
         sql
       end
 
       def add_lock_none(sql)
-        if StrongMigrations.force_lock_none && !StrongMigrations.force_instant_ddl && sql.upcase.include?('ALTER TABLE')
+        if StrongMigrations.force_lock_none && !StrongMigrations.force_instant_ddl && is_target_for_check_online_ddl?(sql)
+          sql = sql.gsub(/,\s*LOCK\s*=\s*\w+\s*/i, '').squeeze(' ').strip
           sql = "#{sql.chomp(";")}, LOCK=NONE;"
         end
         sql
+      end
+
+      def is_target_for_check_online_ddl?(sql)
+        sql.upcase.include?('ALTER TABLE')
+          || sql.upcase.include?('CREATE INDEX')
+          || sql.upcase.include?('CREATE UNIQUE INDEX')
+          || sql.upcase.include?('CREATE FULLTEXT INDEX')
+          || sql.upcase.include?('CREATE SPATIAL INDEX')
+          || sql.upcase.include?('ALTER INDEX')
+          || sql.upcase.include?('DROP INDEX')
       end
     end
   end
